025b95dcd50f918440b52e9b8305ba9d74330b38
Mr-cody-77 <sonu77mahata@gmail.com>
2025-01-28 14:03:07 +0530

    setting.py changes

--- backend/settings.py
+++ backend/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = [
