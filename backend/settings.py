fe9fa999d7f07ea797974fd228a3de616d0c64dd
Mr-cody-77 <sonu77mahata@gmail.com>
2025-04-10 19:57:49 +0530

    minor fix 3

--- backend/settings.py
+++ backend/settings.py
@@ -170,7 +170,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-        # 'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_RENDERER_CLASSES':[
         'rest_framework.renderers.JSONRenderer',
