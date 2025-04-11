e90e9ba8a2ee2a693ac5943eb3abe2301dc6c800
Mr-cody-77 <sonu77mahata@gmail.com>
2025-04-11 18:55:53 +0530

    minor fixes

--- AuditionForm/views.py
+++ AuditionForm/views.py
@@ -36,7 +36,7 @@ from django.http import Http404
 #     } 
 
 class AuditionDataView(APIView):
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     def post(self, request):
         serializer = AuditionDataSerializer(data=request.data)
         if serializer.is_valid():
